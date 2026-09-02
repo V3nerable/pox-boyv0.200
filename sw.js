4c574757377e549ea269a352138213b249d6ae17
V3nerable <43313245+V3nerable@users.noreply.github.com>
2026-09-02 15:06:41 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pipboy-cache-v199';
+const CACHE_NAME = 'pipboy-cache-v200';
 // v0.53: radio packs live in their own bucket -- app updates must NEVER wipe them
 const RADIO_CACHE = 'pox-radio-v1';
 
